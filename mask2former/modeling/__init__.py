f5572ca4b77d75f56af340d3d3634f181017bc17
AICoder12 <fangtiyu123@gmail.com>
2025-07-27 11:29:57 +0800

    Add files via upload

---
+++ mask2former/modeling/__init__.py
@@ -0,0 +1,5 @@
+from .backbone.resnet import build_resnet_backbone_new
+from .pixel_decoder.fpn import BasePixelDecoder
+from .pixel_decoder.msdeformattn import MSDeformAttnPixelDecoder
+from .meta_arch.mask_former_head import MaskFormerHead
+from .meta_arch.per_pixel_baseline import PerPixelBaselineHead, PerPixelBaselinePlusHead
